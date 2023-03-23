2f9131bcbbde89dd2d9def1e8972c977578acd4c
Thor Löfgren <lofgrenthor@gmail.com>
2023-03-23 21:20:08 +0100

    feat(profile page): Basic setup
    - Add an .env file with the url to the server/api.
    
    - Create a basic page on the “/profile” route with links to the
      “/tournaments”, “/classes”, and “account-details” routes.

--- src/Pages/Profile/ProfilePage.jsx
+++ src/Pages/Profile/ProfilePage.jsx
@@ -1,1 +1,25 @@
-export const ProfilePage = () => {};
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context";
+
+import styles from "./ProfilePage.module.css";
+
+export const ProfilePage = () => {
+  const { logOutUser } = useContext(AuthContext);
+
+  return (
+    <div className={styles.profile}>
+      <div className={styles.profile__navLinks}>
+        <Link className={styles.profile__navLinks__item} to="/tournaments">
+          Tournaments
+        </Link>
+        <Link className={styles.profile__navLinks__item} to="/classes">
+          Classes
+        </Link>
+        <Link className={styles.profile__navLinks__item} to="/account-details">
+          Account Details
+        </Link>
+      </div>
+    </div>
+  );
+};
