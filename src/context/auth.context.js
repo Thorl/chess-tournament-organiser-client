2c471e2c4021c2a24c21dc583fcabd0211417e84
Thor Löfgren <lofgrenthor@gmail.com>
2023-04-08 11:04:15 +0200

    style(matches component)
    - Style the "Finished tournament" message. Move it above the matches grid.
    
    - Make the page look good even running a tournament with a large amount
    of players (10).

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
 import React, { useState, useEffect } from "react";
+
 import { useNavigate } from "react-router-dom";
+import { API_URL } from "../constants/API_URL";
 
 export const AuthContext = React.createContext();
 
@@ -20,7 +21,7 @@ export const AuthProviderWrapper = (props) => {
 
     if (storedAuthToken) {
       try {
-        await axios.get("http://localhost:5005/auth/verify", {
+        await axios.get(`${API_URL}/auth/verify`, {
           headers: { Authorization: `Bearer ${storedAuthToken}` },
         });
 
